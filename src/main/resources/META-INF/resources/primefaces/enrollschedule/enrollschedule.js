8098583a674bcde99ff098cc6415310e6bc28420
Piotr Turek <piotr.turu.turek@gmail.com>
2013-02-17 19:28:51 +0100

    Now, reacting to click on an event only if its interactive

--- src/main/resources/META-INF/resources/primefaces/enrollschedule/enrollschedule.js
+++ src/main/resources/META-INF/resources/primefaces/enrollschedule/enrollschedule.js
@@ -5359,7 +5359,7 @@ PrimeFaces.widget.EnrollSchedule = PrimeFaces.widget.BaseWidget.extend({
         this.cfg.eventClick = function(calEvent, jsEvent, view) {
             if(_self.cfg.behaviors) {
                 var eventSelectBehavior = _self.cfg.behaviors['eventSelect'];
-                if(eventSelectBehavior) {
+                if(eventSelectBehavior && calEvent.interactive) {
                     var ext = {
                         params: [
                             {name: _self.id + '_selectedEventId', value: calEvent.id}
